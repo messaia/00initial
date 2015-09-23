cf7cc870816bcaea7d2e3efebdaf1b8148ca1da2
Michael Messaia <miquel.luque.rodriguez@gmail.com>
2015-09-23 17:48:52 +0200

    In Process

---
+++ M03 - Ex 8.2/src/car/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package car;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+/**
+ * @author Alpaca King (miquel.luque.rodriguez@gmail.com)
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		List<Car> cars = new ArrayList<>(5);
+		cars.add(new Car("Alfa Romeo","Giulia",2900,6));
+		cars.add(new Car("Dacia","Sandero",1200,4));
+		cars.add(new Car("Ford","Focus",2000,4));
+		cars.add(new Car("Opel","Insignia",2200,4));
+		cars.add(new Car("Seat","Ibiza",1600,4));
+		System.out.println("The car in position 2 has a fiscal horsepower of: " + cars.get(2).getFiscalHorsepower());
+	}
+
+}
