1a1fba1023dcf65fff2193765256c41484329d89
Alpaca King <miquel.luque.rodriguez@gmail.com>
2015-09-25 19:21:05 +0200

    Finished

--- M03 - Ex 8.2/src/car/Main.java
+++ M03 - Ex 8.2/src/car/Main.java
@@ -21,7 +21,37 @@ public class Main {
 		cars.add(new Car("Ford","Focus",2000,4));
 		cars.add(new Car("Opel","Insignia",2200,4));
 		cars.add(new Car("Seat","Ibiza",1600,4));
-		System.out.printf("The car in position 2 has a fiscal horsepower of: %.2f", cars.get(2).getFiscalHorsepower());
+		System.out.printf("The car in position 2 has a fiscal horsepower of: %.2f\n", cars.get(2).getFiscalHorsepower());
+		cars.add(new Car("Hyundai","Atos",1500,3));
+		cars.add(new Car("Ford","Focus",2000,4));
+		cars.add(new Car("Ford","Focus",2000,4));
+		System.out.println("Yes, it is possible to add duplicates.");
+		cars.add(1,new Car("Audi","A8",4600,8));
+		System.out.println("Yes, it is possible to add a car in position 1.");
+		cars.set(4, new Car("Seat","Mii",1000,3));
+		int counter = 0;
+		for (Car car : cars) {
+			if (car.getBrand().equals("Ford") && car.getModel().equals("Focus")) {
+				System.out.println("The car Ford Focus is in position: " + counter + "\n\n");
+			}
+			counter++;
+		}
+		System.out.println("*************************FOR LOOP*************************");
+		for ( int i = 0 ; i < cars.size() ; i++ ){
+			System.out.println(cars.get(i).getBrand() + "\t" + cars.get(i).getModel() + "\t" + cars.get(i).getNCylinders() + "\t" + cars.get(i).getEngDisplacement() + "\t" + cars.get(i).getFiscalHorsepower());
+		}
+		System.out.println("**********************************************************\n\n");
+		System.out.println("*************************FOR EACH*************************");
+		for ( Car car : cars ){
+			System.out.println(car.getBrand() + "\t" + car.getModel() + "\t" + car.getNCylinders() + "\t" + car.getEngDisplacement() + "\t" + car.getFiscalHorsepower());
+		}
+		System.out.println("**********************************************************\n\n");
+		System.out.println("*************************ITERATOR*************************");
+		Iterator<Car> iterator = cars.iterator();
+		while(iterator.hasNext())  {
+			Car car = iterator.next();
+			System.out.println(car.getBrand() + "\t" + car.getModel() + "\t" + car.getNCylinders() + "\t" + car.getEngDisplacement() + "\t" + car.getFiscalHorsepower());
+		}
+		System.out.println("**********************************************************\n\n");
 	}
-
 }
